b8f30ec9ac6116e97358aea5200ccdce9655113f
Adam La Morre <alamorre@gmail.com>
2022-12-12 10:51:49 -0800

    Comments for client react

--- client-react/src/chatsPage/index.js
+++ client-react/src/chatsPage/index.js
@@ -4,9 +4,9 @@ const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh" }}>
       <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        username={props.user.username}
-        secret={props.user.secret}
+        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID} // 5d498a31-cd23-42b7-b367-4fcc9463bd2f
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
         style={{ height: "100%" }}
       />
     </div>
