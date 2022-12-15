b37cf8ff1d813e07662dbfe02480eb0f0d86faf9
Adam La Morre <alamorre@gmail.com>
2022-12-14 16:34:02 -0800

    hardcode project ID

--- client-react/src/chatsPage.js
+++ client-react/src/chatsPage.js
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh" }}>
       <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID} // 5d498a31-cd23-42b7-b367-4fcc9463bd2f
+        projectId="5d498a31-cd23-42b7-b367-4fcc9463bd2f"
         username={props.user.username} // adam
         secret={props.user.secret} // pass1234
         style={{ height: "100%" }}
