ad48d2ca5fd12cd06fb3947b7df40d5fb61f892b
Adam La Morre <alamorre@gmail.com>
2022-12-15 19:04:47 -0800

    CSS patch on auth

--- client-react/src/authPage.js
+++ client-react/src/authPage.js
@@ -90,7 +90,7 @@ const AuthPage = (props) => {
 
       <style>{`
       .login-page { width: 100vw; height: 100vh; padding-top: 6vw; background: linear-gradient(180deg, rgba(117,84,160,1) 7%, rgba(117,84,160,1) 17%, rgba(106,95,168,1) 29%, rgba(99,103,174,1) 44%, rgba(87,116,184,1) 66%, rgba(70,135,198,1) 83%, rgba(44,163,219,1) 96%, rgba(22,188,237,1) 100%, rgba(0,212,255,1) 100%); }
-      .card { width: 200px; position: relative; left: calc(50vw - 150px); text-align: center; }
+      .card { width: 200px; position: relative; left: calc(50vw - 100px); text-align: center; }
       .title { padding-top: 32px; font-size: 22px; color: white; font-weight: 700; }
       input { width: calc(100% - 16px); margin-top: 12px; padding: 8px; background-color: #e6f7ff; outline: none; border: 1px solid #e6f7ff; }
       button { margin-top: 12px; width: 100%; padding: 8px; }
