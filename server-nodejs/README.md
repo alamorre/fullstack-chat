9454d641e5dadac4089f9bc387717493de981d13
Adam La Morre <alamorre@gmail.com>
2022-12-15 18:58:06 -0800

    Basec NodeJS support

--- server-nodejs/README.md
+++ server-nodejs/README.md
@@ -1,1 +1,14 @@
-# Coming soon!
+## NodeJS Server Setup
+
+```
+npm init
+npm install express axios cors
+npm install --save-dev nodemon
+# Add: "scripts": { "start": "nodemon index.js" },
+echo node_modules/ > .gitignore
+touch index.js
+# Add the code
+npm run start
+```
+
+Edit the package.json file
---
+++ server-nodejs/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const CHAT_ENGINE_PROJECT_ID = "";
+const CHAT_ENGINE_PRIVATE_KEY = "";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
+app.listen(3001);
